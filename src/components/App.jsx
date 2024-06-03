b06cdf4097ba1c4887914d1601f046b1098ea85a
bohdam1 <mochuradb@gmail.com>
2024-06-03 22:53:52 +0300

    finish task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,71 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { FormBook } from './form/Form'; 
+import { ContactsList } from './ContactsList/ContactsList';
+import { nanoid } from 'nanoid';
+import { Filter } from "./Filter/Filter";
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+    name: '',
+    number: ''
+  }
+ 
+
+
+  handleChange = (event) => {
+    const { name, value } = event.target;  
+    this.setState({ [name]: value });  
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { name, number, contacts } = this.state;
+
+    // Перевірка чи ім'я вже присутнє у списку контактів
+    const isDuplicate = contacts.some(contact => contact.name.toLowerCase() === name.toLowerCase());
+    if (isDuplicate) {
+      alert(`${name} is already in the contact list.`);
+      return;
+    }
+
+    const newContact = { id: nanoid(), name, number };
+
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, newContact],
+      name: '',
+      number: ''
+    }));
+  };
+
+  handleFilter = (event) => {
+    this.setState({ filter: event.target.value });
+  };
+
+  handleDelete = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id)
+    }));
+  };
+
+
+
+
+  render() {
+    const { name, number, filter, contacts } = this.state;
+    const filteredContacts = contacts.filter(contact => 
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <FormBook name={name} number={number} onSubmit={this.handleSubmit} onChange={this.handleChange} />
+        <h2>Contacts</h2>
+        <Filter onSearch={this.handleFilter}/>
+        <ContactsList contacts={filteredContacts} onDelete={this.handleDelete}/>
+      </div>
+    );
+  }
+}
---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,9 @@
+import css from "./filter.module.css"
+
+export const Filter = ({onSearch})=>{
+    return(
+        <div className={css.container}>
+            <input type="text" name="filter" onChange={onSearch} className={css.input}/>
+        </div>
+    )
+}
